d98b8ece0a19e082e4ea70c631a4dd0ea4574d43
Vatamaniucdenis88 <vatamaniuc88@gmail.com>
2020-05-21 10:33:18 +0100

    Updates

---
+++ precache-manifest.a72bc3351b1aa708a659fb4315431de1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e809c9e10c61d0b90bbef44261ca219",
+    "url": "/movieapi/index.html"
+  },
+  {
+    "revision": "2fecabcec9f496381e13",
+    "url": "/movieapi/static/css/main.96b9ae23.chunk.css"
+  },
+  {
+    "revision": "8686e30fef4a6518d9dc",
+    "url": "/movieapi/static/js/2.416dfc8c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movieapi/static/js/2.416dfc8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fecabcec9f496381e13",
+    "url": "/movieapi/static/js/main.1f6570eb.chunk.js"
+  },
+  {
+    "revision": "e89fbec67ca752569858",
+    "url": "/movieapi/static/js/runtime-main.4826f90f.js"
+  },
+  {
+    "revision": "8d2d203eb46e96b46ed002e5cf30398f",
+    "url": "/movieapi/static/media/cover.8d2d203e.png"
+  }
+]);
